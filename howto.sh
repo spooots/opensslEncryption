e9ba9b1debd185cf80e1a6e087676141b8683163
Andreas Schedel <andreas.schedel@harman.com>
2017-11-13 07:51:00 +0100

    messages optimized

--- howto.sh
+++ howto.sh
@@ -31,10 +31,10 @@ INFO "> generate public key RECEIVER"
 openssl rsa -in privateR.pem -passin file:passphrase.txt -pubout -out publicR.pem || exit $?
 
 INFO ""
-INFO "generate a random key file"
+INFO "> generate a random key file"
 openssl rand 32 -out keyfile || exit $?
 
-INFO "SENDER and RECEIVER have to exchange their public keys in a safe way"
+INFO "SENDER and RECEIVER have now to exchange their public keys in a safe way"
 
 ################################################################################
 ## Encryption -> on sender side
@@ -44,10 +44,10 @@ INFO "SENDER:"
 INFO "   Encrypt File with AES Key"
 openssl enc -aes-256-cbc -salt -in message.txt -out message.enc -pass file:keyfile || exit $?
 
-INFO "   Encrypt AES Key with RSA Public Key"
+INFO "   Encrypt AES Key with Receivers RSA public key"
 openssl rsautl -encrypt -inkey publicR.pem -pubin -in keyfile -out keyfile.enc || exit $?
 
-INFO "   Generate a signature for the message.txt"
+INFO "   Generate a signature for the message.txt with Senders private key"
 openssl dgst -sha256 -sign privateS.pem -out signature.txt message.txt  || exit $?
 
 # You can now transmit the file.enc, aesKey.txt.crypted, signature.txt and the public.pem via email or something similar. Dont send the private.p# zem!
@@ -58,13 +58,13 @@ openssl dgst -sha256 -sign privateS.pem -out signature.txt message.txt  || exit 
 ################################################################################
 INFO ""
 INFO "RECEIVER:"
-INFO "   Decrypt AES key with RSA private key"
+INFO "   Decrypt AES key with Receivers RSA private key"
 openssl rsautl -decrypt -inkey privateR.pem -in keyfile.enc -out keyfile.decrypted -passin file:passphrase.txt || exit $?
 
 INFO "   Decrypt file with AES Key"
 openssl enc -d -aes-256-cbc -in message.enc -out message.txt.decrypted -pass file:keyfile.decrypted || exit $?
 
-INFO "   Verify the signature for the recieved file.txt and the signature.txt"
+INFO "   Verify the signature for the recieved file.txt using the senders public key and signature.txt"
 openssl dgst -sha256 -verify publicS.pem -signature signature.txt message.txt || exit $?
 # in case of success: prints "Verified OK"
 # in case of failure: prints "Verification Failure"
